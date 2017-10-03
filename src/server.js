2628633d2267d4ae87b0b166bd4be43c4bfcac3e
KyleScharnhorst <scharkr1@uw.edu>
2017-10-03 04:15:52 -0700

    Add: basic restful server api.

---
+++ src/server.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+//setup body parser
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//setup port
+var port = process.env.port || 8080;
+
+//setup router
+var router = express.Router();
+
+//
+// SCRIPT EXECUTION
+//
+function callScript() {
+    var util = require('util'),
+        exec = require('child_process').exec,
+        child;
+
+    child = exec('dir', // command line argument directly in string
+        function (error, stdout, stderr) {      // one easy function to capture data/errors
+            console.log('stdout: ' + stdout);
+            console.log('stderr: ' + stderr);
+            if (error !== null) {
+                console.log('exec error: ' + error);
+            }
+        });
+}
+
+//
+// ROUTES
+//
+
+//middleware used to catch before being router to handling function.
+router.use(function(req, res, next){
+    // console.log('req: ' + req);
+    // console.log('res: ' + res);
+    // console.log('next: ' + next);
+    console.log('Middleware called.');
+    next();
+});
+
+router.get('/', function(req, res) {
+    res.json({ message: 'Restful api used for searching local NAS.' });
+});
+
+router.get('/update', function (req, res) {
+    console.log('Updating');
+    //call update script
+    res.json({ message: 'Update finished.' });
+});
+
+router.get('/search/:search_val', function (req, res) {
+    console.log('Searching');
+    //call search script
+    res.json({ message: 'Search finished.' });
+});
+
+//register routes
+app.use('/api', router);
+// app.use('/api/search');
+
+//
+// START SERVER
+//
+app.listen(port);
+console.log('Starting server on port: ' + port);
