22d394aead18a049139eeb217e9b0c06046e9973
KyleScharnhorst <scharkr1@uw.edu>
2017-10-02 18:26:08 -0700

    Add: update placeholder and update section.

--- src/App.js
+++ src/App.js
@@ -3,19 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    updateDB(e) {
+        e.preventDefault();
+        console.log('Updating DB.');
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Search NAS Utility</h1>
+                </header>
+                <p className="App-update">
+                    To manually update the search database click&nbsp;
+                    <button onClick={this.updateDB}>
+                        here
+                    </button>. Keep in mind this is automatically done once a day.
+                </p>
+                <p className="App-search">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
